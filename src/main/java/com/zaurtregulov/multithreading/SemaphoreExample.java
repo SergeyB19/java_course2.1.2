4d5299d4f45d05eeaa7e29c029229c38907f285b
SergeyB19 <my.h.m@yandex.ru>
2024-03-02 10:40:21 +0300

    Многопоточность(Semaphore)

---
+++ src/main/java/com/zaurtregulov/multithreading/SemaphoreExample.java
@@ -0,0 +1,41 @@
+package com.zaurtregulov.multithreading;
+
+import java.util.concurrent.Semaphore;
+
+public class SemaphoreExample {
+    public static void main(String[] args) {
+        Semaphore callBox = new Semaphore(2);
+        new Person("Zaur", callBox);
+        new Person("Oleg", callBox);
+        new Person("Elena", callBox);
+        new Person("Victor", callBox);
+        new Person("Marina", callBox);
+
+
+    }
+}
+
+class Person extends Thread {
+    String name;
+    private Semaphore callBox;
+
+    public Person(String name, Semaphore callBox) {
+        this.name = name;
+        this.callBox = callBox;
+        this.start();
+    }
+
+    public void run() {
+        try {
+            System.out.println(name + " ждет ...");
+            callBox.acquire();
+            System.out.println(name + " пользуется телефоном");
+            sleep(2000);
+            System.out.println(name + " завершил звонок");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            callBox.release();
+        }
+    }
+}
