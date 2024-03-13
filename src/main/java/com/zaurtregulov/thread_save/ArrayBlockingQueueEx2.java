c6f212b00ae52b24da7e315c553820805dc5e10c
SergeyB19 <my.h.m@yandex.ru>
2024-03-13 10:43:02 +0300

    Многопоточность(ArrayBlockingQueueEx2)

---
+++ src/main/java/com/zaurtregulov/thread_save/ArrayBlockingQueueEx2.java
@@ -0,0 +1,40 @@
+package com.zaurtregulov.thread_save;
+
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class ArrayBlockingQueueEx2 {
+    public static void main(String[] args) {
+
+        ArrayBlockingQueue<Integer> arrayBlockingQueue
+                = new ArrayBlockingQueue<>(4);
+
+        //Producer
+        new Thread(() -> {
+            int i = 0;
+            while (true) {
+                try {
+                    arrayBlockingQueue.put(++i);
+                    System.out.println("Producer dobavil: " + i + " " + arrayBlockingQueue);
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
+
+        //Consumer
+        new Thread(() -> {
+            while (true) {
+                try {
+                    Integer j = arrayBlockingQueue.take();
+                    System.out.println("Consumer zabral: " + j + " " + arrayBlockingQueue);
+                    Thread.sleep(9000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
+
+    }
+}
+
