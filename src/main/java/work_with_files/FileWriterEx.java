4fe7031d5d3dfbfa6ac4c2b8cebf84fa7b5425fa
SergeyB19 <my.h.m@yandex.ru>
2024-03-14 10:21:22 +0300

    Многопоточность(FileWriter)

---
+++ src/main/java/work_with_files/FileWriterEx.java
@@ -0,0 +1,29 @@
+package work_with_files;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriterEx {
+    public static void main(String[] args) throws IOException {
+        String rubai = "Много лет размышлял я над жизнью земной.\n" +
+                "Непонятного нет для меня под луной.\n" +
+                "Мне известно, что мне ничего не известно!\n" +
+                "Вот последняя правда, открытая мной.\n";
+        String s = "privet";
+        FileWriter writer = null;
+        try {
+//            writer = new FileWriter("C:\\Users\\serge\\Desktop\\test1.txt.txt");
+            writer = new FileWriter("test3.txt",true);
+          /*  for (int i = 0; i < rubai.length(); i++) {
+                writer.write(rubai.charAt(i));
+            }*/
+//            writer.write(rubai);
+            writer.write(s);
+            System.out.println("Done!");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            writer.close();
+        }
+    }
+}
