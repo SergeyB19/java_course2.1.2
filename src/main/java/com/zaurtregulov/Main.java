44c680a000c240b2e5c7e56485db3a24b887c27b
Bayakan <spring.boot@hotmail.com>
2024-03-23 12:13:06 +0300

    IO&&NIO(RandomAccessFile)

--- src/main/java/com/zaurtregulov/Main.java
+++ src/main/java/com/zaurtregulov/Main.java
@@ -1,9 +1,25 @@
 package com.zaurtregulov;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.text.DecimalFormat;
+import java.util.Formatter;
+import java.util.Locale;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.ENGLISH);
 
+        BigDecimal bigDecimal = null;
+        BigDecimal Dwalin = scanner.nextBigDecimal();
+        BigDecimal Balin = scanner.nextBigDecimal();
+        BigDecimal Thorin = scanner.nextBigDecimal();
+
+        BigDecimal totalGold = Dwalin.add(Balin).add(Thorin);
+        System.out.println("totalGold: " + totalGold);
+    }
 }
